752553cd836efb8e7dbed709142586927a2f1ffb
Brian <nbmbestre91@gmail.com>
2023-08-06 21:44:30 +0800

    Updated Clients for search and new button functions

--- ui/src/pages/Clients/index.tsx
+++ ui/src/pages/Clients/index.tsx
@@ -1,30 +1,49 @@
-import { memo, useContext, useEffect } from "react";
-import { Paper, Typography } from "@mui/material";
-import { StateContext } from "../../store/DataProvider";
-import Page from "../../components/Page";
-import ClientTable from "./ClientTable";
-import { getClients } from "../../services/api";
+import { memo, useContext, useEffect, useState } from 'react';
+import { Box, Button, Paper, Typography } from '@mui/material';
+import { StateContext } from '../../store/DataProvider';
+import Page from '../../components/Page';
+import ClientTable from './ClientTable';
+import { getClients, createClient } from '../../services/api';
+import SearchInput from '../../components/SearchInput';
+import ClientDialog from './ClientDialog';
 
 function Clients() {
-  const { state, dispatch } = useContext(StateContext);
-  const { clients } = state;
+	const { state, dispatch } = useContext(StateContext);
+	const { clients } = state;
+	const [clientDialogOpen, setClientDialogOpen] = useState(false);
 
-  useEffect(() => {
-    getClients().then((clients) =>
-      dispatch({ type: "FETCH_ALL_CLIENTS", data: clients })
-    );
-  }, [dispatch]);
+	const openClientDialog = () => {
+		setClientDialogOpen(true);
+	};
 
-  return (
-    <Page>
-      <Typography variant="h4" sx={{ textAlign: "start" }}>
-        Clients
-      </Typography>
-      <Paper sx={{ margin: "auto", marginTop: 3 }}>
-        <ClientTable clients={clients} />
-      </Paper>
-    </Page>
-  );
+	const onNewClientSave = (newClient: IClient) => {
+		createClient(newClient).then((client) => {
+			console.log('client', client);
+			dispatch({ type: 'SAVE_CLIENT', data: client });
+		});
+	};
+
+	useEffect(() => {
+		getClients().then((clients) => dispatch({ type: 'FETCH_ALL_CLIENTS', data: clients }));
+	}, [dispatch, clientDialogOpen]);
+
+	return (
+		<Page>
+			<Typography variant='h4' sx={{ textAlign: 'start' }}>
+				Clients
+			</Typography>
+			<Box sx={{ display: 'flex', justifyContent: 'space-between', margin: 'auto', marginTop: 3 }}>
+				<SearchInput />
+				<Button variant='contained' sx={{ textTransform: 'unset' }} onClick={openClientDialog}>
+					Create new client
+				</Button>
+			</Box>
+			<ClientDialog open={clientDialogOpen} handleClose={setClientDialogOpen} handleSave={onNewClientSave} />
+			<Paper sx={{ margin: 'auto', marginTop: 3 }}>
+				<ClientTable clients={clients} />
+			</Paper>
+		</Page>
+	);
 }
 
 export default memo(Clients);
