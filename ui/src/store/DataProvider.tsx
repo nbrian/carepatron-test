9da7d85f953f074c4a8c67da09bc57e4337df309
Brian <nbmbestre91@gmail.com>
2023-08-06 21:43:31 +0800

    Update DataProvider for new client action

--- ui/src/store/DataProvider.tsx
+++ ui/src/store/DataProvider.tsx
@@ -1,50 +1,49 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer } from 'react';
 
 const initialState: IApplicationState = {
-  clients: [],
+	clients: [],
 };
 
 export const StateContext = createContext<{
-  state: IApplicationState;
-  dispatch: React.Dispatch<Action>;
+	state: IApplicationState;
+	dispatch: React.Dispatch<Action>;
 }>(
-  // @ts-ignore
-  null
+	// @ts-ignore
+	null
 );
 
 export const ACTIONS = {
-  FETCH_ALL_CLIENTS: "FETCH_ALL_CLIENTS",
+	FETCH_ALL_CLIENTS: 'FETCH_ALL_CLIENTS',
+	SAVE_CLIENT: 'SAVE_CLIENTS',
 };
 
 type Action = {
-  type: keyof typeof ACTIONS;
-  data: any;
+	type: keyof typeof ACTIONS;
+	data: any;
 };
 
 const reducer = (state: IApplicationState, action: Action) => {
-  switch (action.type) {
-    case ACTIONS.FETCH_ALL_CLIENTS:
-      return { ...state, clients: action.data };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case ACTIONS.FETCH_ALL_CLIENTS:
+			return { ...state, clients: action.data };
+		case ACTIONS.SAVE_CLIENT:
+			return { ...state, client: action.data };
+		default:
+			return state;
+	}
 };
 
-export default function DataProvider({
-  children,
-}: {
-  children?: React.ReactNode;
-}) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+export default function DataProvider({ children }: { children?: React.ReactNode }) {
+	const [state, dispatch] = useReducer(reducer, initialState);
 
-  return (
-    <StateContext.Provider
-      value={{
-        state,
-        dispatch,
-      }}
-    >
-      {children}
-    </StateContext.Provider>
-  );
+	return (
+		<StateContext.Provider
+			value={{
+				state,
+				dispatch,
+			}}
+		>
+			{children}
+		</StateContext.Provider>
+	);
 }
